ba30e29ad876930aa60b6d1a326d3554c41d8a7e
Helena Moreno Chacón <helenatxu@gmail.com>
2010-07-11 14:44:37 +0200

    Some data test added

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100711110504) do
+ActiveRecord::Schema.define(:version => 20100711114351) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
