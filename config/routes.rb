16a67463a20c76a9d4782f9213921ea81f3f2764
Helena Moreno Chacón <helenatxu@gmail.com>
2010-07-11 12:29:21 +0200

    DBs: places routes users comments --> added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resources :comments
+
+  map.resources :routes
+
+  map.resources :places
+
+  map.resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
