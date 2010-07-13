f69b7b5644ab46caa6863f7c41755d3fac269711
Helena Moreno Chacón <helenatxu@gmail.com>
2010-07-13 18:58:18 +0200

    password and profile test data added to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ActiveRecord::Base
+
+  validates_presence_of :username, :message => 'you must write an user'
+  validates_presence_of :email, :message => 'you must write an email'
+  validates_uniqueness_of :username, :message => 'username already used'
+  validates_uniqueness_of :email, :message => 'email already used'
   
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username, :email
   validates_format_of :photo_url, 
   :with => %r{\.(gif|jpg|png)$}i, 
   :message => 'must be a URL for GIF, JPG ' + 
   'or PNG image.' 
+
+
+  validates_confirmation_of :hashed_password 
+  validate :password_non_blank 
+
+  private 
+  def password_non_blank 
+    errors.add("Password") if hashed_password.blank? 
+  end 
   
-  
-end
+end 
+
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100713161039) do
+ActiveRecord::Schema.define(:version => 20100713194351) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
