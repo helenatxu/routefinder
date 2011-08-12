2660dbc2fee4d0fcf5b2354590fc8f24cc89521d
Helena Moreno Chacon <helenaenred@gmail.com>
2011-08-12 14:13:25 +0200

    more 3

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "ajaxful_rating"
+  config.gem "ajaxful_rating", :version => '2.2.8.2'
   config.gem "authlogic", :version => '<3.0.0'
   config.gem "sqlite3-ruby", :lib => "sqlite3"
 
